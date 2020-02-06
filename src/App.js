4147250c39f41377403644e9b7715e90c7049488
KGood93 <goodreau.kath@gmail.com>
2020-02-05 22:06:31 -0600

    Add Nav Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-    </div>
-  );
+  renderNavRoutes(){
+    return (
+      <>
+        {['/', '/meetupSelection'].map(path => (
+          <Route
+            exact
+            key={path}
+            path={path}
+            component={MeetupSelection}
+          />
+        ))}
+        <Route path='/launch' component={LaunchPage}/>
+        <Route path='/newEvent' component={NewEvent}/>
+        <Route path='/doorCount' component={DoorCounter}/>
+        <Route path='/introCount' component={IntroCounter}/>
+        <Route path='/stats' component={StatsPage}/>
+      </>
+    )
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header>
+          <Header />
+        </header>
+
+        <nav className="AppNav">{this.renderNavRoutes()}</nav>
+
+        <footer>
+          <p>Important Info</p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
