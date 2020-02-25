affec5217784d314f698d0d5a94179789f1c0264
KGood93 <goodreau.kath@gmail.com>
2020-02-25 15:52:11 -0600

    combine doorCounter page and new meeting page

--- src/components/DoorCounter/DoorCounter.js
+++ src/components/DoorCounter/DoorCounter.js
@@ -1,34 +1,74 @@
 import React, {Component} from 'react'
+import StatsForm from '../StatsForm/StatsForm'
 
 class DoorCounter extends Component {
     constructor(props) {
         super(props)
         this.state = {
             clicks: 0,
-            show: true
+            show: true,
+            location: {
+                value: '',
+                touched: false
+            },
+            redirect: false
         }
+    }
+
+    updateLocation(meetingLocation) {
+        this.setState({location: {value: meetingLocation, touched: true}})
     }
 
     incrementCount = () => {
         this.setState({clicks: this.state.clicks + 1})
     }
 
+    handleSubmit = event => {
+        event.preventDefault()
+        const {location, clicks} = this.state
+        console.log(location.value)
+        console.log(clicks.value)
+
+        const meetingDetails = {
+            //date: date.value,
+            location: location.value,
+            count: clicks.value
+        }
+
+        console.log(meetingDetails)
+    }
+
     render() {
         return (
             <div className="doorCounter">
-                <h2>Door Count</h2>
-                <h2>{this.state.clicks}</h2>
-                <div className="doorButtons">
-                    <button 
-                        type="button" 
-                        className="addButton"
-                        onClick={this.incrementCount}
-                    >
-                        +
-                    </button>
-                    <br/>
-                    <button type="button" className="mainButton">Next</button>
+                <StatsForm className="meeting" onSubmit={this.handleSubmit}>
+                        <div className="newLocation">
+                            <h2 className="inputLabel">Location</h2>
+                            <input 
+                                type="text" 
+                                name="newDate" 
+                                className="input" 
+                                onChange={e => this.updateLocation(e.target.value)}
+                            />
+                        </div>
+
+                
+                <div className="count">
+                    <h2>Number of Attendees</h2>
+                    <h2>{this.state.clicks}</h2>
+                    <div className="doorButtons">
+                        <button 
+                            type="button" 
+                            className="addButton"
+                            onClick={this.incrementCount}
+                        >
+                            +
+                        </button>
+                        <br/>
+                        <button type="submit" className="mainButton">Next</button>
+                    </div>
                 </div>
+                </StatsForm>
             </div>
         )
     }
