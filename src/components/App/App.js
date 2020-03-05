0cd3fd800e9b1c6c86760f319db9392a2ae21e66
KGood93 <goodreau.kath@gmail.com>
2020-03-05 15:44:52 -0600

    Add api calls and set context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,86 @@ import MeetupSelection from '../MeetupSection/MeetupSection'
 import LaunchPage from '../LaunchPage/LaunchPage'
 import DoorCounter from '../DoorCounter/DoorCounter'
 import StatsPage from '../StatsPage/StatsPage'
+import config from '../../config'
+import Context from '../../context'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meetups: [],
+      stats: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMeetups();
+    this.fetchStats();
+  }
+
+  fetchMeetups() {
+    const meetupUrl = `${config.API_ENDPOINT}/meetups`
+    const meetupOptions = {
+      method: 'GET', 
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }
+
+    fetch(meetupUrl, meetupOptions)
+      .then(res => {
+        if(res.ok) {
+          return res.json()
+        }
+        else {
+          throw new Error('Something went wrong loading meetup options')
+        }
+      })
+      .then(data => {
+        //console.log(data)
+        this.setState({meetups: data})
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        })
+      })
+  }
+
+  fetchStats() {
+    const statsUrl = `${config.API_ENDPOINT}/stats`
+    const statsOptions = {
+      method: 'GET', 
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }
+
+    fetch(statsUrl, statsOptions)
+      .then(res => {
+        if(res.ok) {
+          return res.json()
+        }
+        else {
+          throw new Error('Something went wrong loading stats')
+        }
+      })
+      .then(data => {
+        //console.log(data)
+        this.setState({stats: data})
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        })
+      })
+  }
 
   renderNavRoutes(){
     return (
       <>
-        {['/', '/meetupSelection'].map(path => (
+        {['/', '/meetups'].map(path => (
           <Route
             exact
             key={path}
@@ -28,7 +100,15 @@ class App extends Component {
   }
 
   render() {
+    const value = {
+      meetups: this.state.meetups,
+      stats: this.state.stats
+    }
+    
+    //console.log(value)
+
     return(
+      <Context.Provider value={value}>
       <div className="App">
         <header>
           <Header />
@@ -40,6 +120,7 @@ class App extends Component {
           <p>Important Info</p>
         </footer>
       </div>
+      </Context.Provider>
     );
   }
 }
