102099bf98d20da78b3f02bddeadf4dbc04c5612
KGood93 <goodreau.kath@gmail.com>
2020-03-13 10:17:58 -0500

    Refresh

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,42 +4,13 @@ import { Link } from 'react-router-dom'
 import './Header.css'
 
 export default class Header extends Component {
-  //handleLogoutClick = () => {
-  //  TokenService.clearAuthToken()
-  //}
 
-  //renderLogoutLink() {
-  //  return (
-  //    <div className='Header_logged-in'>
-  //      <Link
-  //        onClick={this.handleLogoutClick}
-  //        to='/'>
-  //        Logout
-  //      </Link>
-  //    </div>
-  //  )
-  //}
-
-  //renderLoginLink() {
-  //  return (
-  //    <div className='Header_not-logged-in'>
-  //      <Link
-  //        to='/registration'>
-  //        Register
-  //      </Link>
-  //      <Link
-  //        to='/login'>
-  //        Log in
-  //      </Link>
-  //    </div>
-  //  )
-  //}
 
   render() {
     return (
       <nav className='Header'>
         <h1>
-          <Link to='/'>
+          <Link to='/' refresh="true">
             {' '}
             Circle Time Stats
             </Link>
